ccbb381f7297d83b67aedb25346af0b3d691808f
kurt <kurt@kurt-ThinkPad-T440>
2017-03-02 11:48:22 -0500

    Added javadoc comments

--- Homework3/src/doublyLinkedList/DoublyLinkedList.java
+++ Homework3/src/doublyLinkedList/DoublyLinkedList.java
@@ -1,34 +1,59 @@
 package doublyLinkedList;
 
 public class DoublyLinkedList {
-
+	// instance variables
 	private Node header;
 	private Node trailer;
 	private int size;
 
+	/**
+	 * Constructor for an empty list
+	 */
 	public DoublyLinkedList() {
 		header = new Node(null, null, null);
 		trailer = new Node(null, header, null);
 		header.setNext(trailer);
 	}
 
+	/**
+	 * gets size
+	 * 
+	 * @return int
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * if empty returns true else returns false
+	 * 
+	 * @return boolean
+	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	
-	public String first(){
-		if(isEmpty()) return null;
+
+	/**
+	 * gets element of first node in list without deleting it
+	 * 
+	 * @return String
+	 */
+	public String first() {
+		if (isEmpty())
+			return null;
 		return header.getNext().getElement();
 
 	}
 
-	public String last(){
-		if(isEmpty()) return null;
+	/**
+	 * gets the element in the last node without deleting it
+	 * 
+	 * @return String
+	 */
+	public String last() {
+		if (isEmpty())
+			return null;
 		return trailer.getPrev().getElement();
 	}
-	
+
 }
